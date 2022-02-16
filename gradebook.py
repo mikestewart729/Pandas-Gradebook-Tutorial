15d5547c9b52fe3f718fc36cb7c233e702b6ac54
Mike <mikestewart729@gmail.com>
2022-02-16 09:13:17 -0500

    Initial commit

---
+++ gradebook.py
@@ -0,0 +1,43 @@
+# gradebook.py
+
+"""Calculate student grades by combining data from many sources.
+
+Using pandas, this script combines data from the:
+
+* Roster
+* Homework & Exam grades
+* Quiz grades
+
+to calculate final grades for a class.
+"""
+
+from pathlib import Path
+import pandas as pd
+
+HERE = Path(__file__).parent
+DATA_FOLDER = HERE / "data"
+
+roster = pd.read_csv(
+    DATA_FOLDER / "roster.csv",
+    converters={"NetID": str.lower, "Email Address": str.lower},
+    usecols=["Section", "Email Address", "NetID"],
+    index_col="NetID",
+)
+
+hw_exam_grades = pd.read_csv(
+    DATA_FOLDER / "hw_exam_grades.csv",
+    converters={"SID": str.lower},
+    usecols=lambda x: "Submission" not in x,
+    index_col="SID",
+)
+
+quiz_grades = pd.DataFrame()
+for file_path in DATA_FOLDER.glob("quiz_*_grades.csv"):
+    quiz_name = " ".join(file_path.stem.title().split("_")[:2])
+    quiz = pd.read_csv(
+        file_path,
+        converters={"Email": str.lower},
+        index_col=["Email"],
+        usecols=["Email", "Grade"],
+    ).rename(columns={"Grade": quiz_name})
+    quiz_grades = pd.concat([quiz_grades, quiz], axis=1)
